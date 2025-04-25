96ff3b23c41526f1f7b03da15a484cb0f030e246
Breanna <w0459556@campus.nscc.ca>
2025-04-25 14:22:01 -0300

    feat: added assignment 2 and required asset(s)

--- assign2.php
+++ assign2.php
@@ -1,4 +1,54 @@
 <?php $assignname = "Prog2007 - Assignment 2" ?>
 <?php include_once("components/assign-body-above.php"); mt_srand(); ?>
+<h4>Assignment 2 - Conditionals, Arrays and Functions</h4>
+<p>
+Write a C program that identifies leap years with proper validation and displays results in a formatted table.
+</p>
 
+<h5>TASK REQUIREMENTS:</h5>
+<h6>Part 1:</h6>
+<ul>
+    <li>Create Boolean leap year function in leapyear.c</li>
+    <li>Function prototype in leapyear.h</li>
+</ul>
+
+<h6>Part 2:</h6>
+<ul>
+    <li>Prompt for and validate year input</li>
+    <li>Check if leap year and display result</li>
+    <li>Loop until user enters 'N' to stop</li>
+</ul>
+
+<h6>Part 3:</h6>
+<ul>
+    <li>Generate Boolean table for years 1-2022</li>
+    <li>Print leap years with one century per line</li>
+</ul>
+
+<h6>General Requirements:</h6>
+<ul>
+    <li>Organize files in src/ and inc/ folders</li>
+    <li>Include clear comments and consistent formatting</li>
+    <li>Provide clear user feedback</li>
+</ul>
+
+<h5>SAMPLE OUTPUT</h5>
+<div class="row">
+    <div class="col-lg-8 image-container">
+        <p class="mb-0 pb-0">Sample output showing leap years by century:</p>
+        <img src="img/A2-1.png" alt="Leap Year Table Output" class="lab-image" style="max-width: 800px;">
+    </div>
+</div>
+
+<h5>SUBMISSION INSTRUCTIONS</h5>
+<ul>
+    <li>Submit video screen capture demonstrating:
+        <ul>
+            <li>CLion and GitBash usage</li>
+            <li>Code review with audio explanation</li>
+            <li>Completion of Video Submission Checklist items</li>
+        </ul>
+    </li>
+    <li>Post video file or link to Brightspace before deadline</li>
+</ul>
 <?php include_once("components/assign-body-below.php"); mt_srand(); ?>
